a810dc1d76e0cfd19b777f054841dfedcecc8f1e
Abd Alrhman Allam <abdalrhmanallam97@gmail.com>
2022-07-03 03:22:41 +0200

    More Information in Array&String Functions

--- 7_ArrayFunctions.php
+++ 7_ArrayFunctions.php
@@ -1,9 +1,84 @@
 <?php
 /* --------- Array Functions -------- */
+/*
+-array_change_key_case(Array[Required], Case[Optional])=>Changes The Case Of All Keys In An Array
+    Case { [CASE_LOWER => Default] , [CASE_UPPER] }
+- array_count_values(Array[Required])=>Counts All The Values Of An Array without repetition
+    [A=>3, B=>1 , C=>0]  number of repeat for every element
+- array_reverse(Array[Required], Preserve[Optional])=>Reverse Array Elements with handling indexes
+    if i want every element reversed with his index put in Preserve ->True
+        print_r(array_reverse($family, True));
+- count(Array[Required], Mode[Optional])
+    Mode [0 => (Default) Does Not Count Elements in Multidimensional Arrays, 1 => Count Elements in Multidimensional Arrays and Multidimensional Array herself]
+        echo count($fruits);
+- in_array(Search[Required], Array[Required], Type[Optional]) =>Checks If A Value Exists In An Array not intersted in datatype
+    if i want to identical of Search and Array element put in the Type -> True
+      $search = ["1", 2, 3, 4];
+      if (in_array(1, $search, True))  -> False because Search and Array not Identical
+        echo "The Element Is Found";
+- array_key_exists(Key[Required], Array[Required]) => Check If Key Is Exists in associative array 
+    Return Boolean Value.
+
+- array_keys(Array[Required], Value[Optional], Type[Optional])=>Return The Array Keys in indexed Array
+    if i want to get keys of specific value put the Value parameter
+    Type=>[ False (Default) -> No Checking For data Type, True -> Check For Type]
+        $friends = ["Osama", "Ahmed", "Sameh", "Mahmoud", "Gamal", "Osama", "Eman", 1, "1"];
+        print_r(array_keys($friends, 1, True)); output [[0]=> 7]-> get Number 1 only because Type is true
+- array_values(Array[Required]) => Return All The Values Of An Array
+
+- array_pad(Array[Required], Size[Required], Value[Required])=>Pad Array To The Specified Length With A Value
+    Negative Value Add Elements Before Original Items
+    If Size < Array Length Nothing Will Be Deleted
+
+-Every Array Has An Internal Pointer To Its "Current" Element,Which Is Initialized To The First Element.
+    - current(Array[Required]) => Return The Current Element In An Array
+    - next(Array[Required]) => Advance The Internal Pointer
+    - prev(Array[Required]) => Rewind The Internal Pointer
+    - reset(Array[Required]) => Put The Internal Pointer On First Element
+    - end(Array[Required]) => Put The Internal Pointer On Last Element
+
+- array_merge(Array[Required], Other_Array/s[Optional])=>Merge One Or More Arrays
+    Later Array Key With The Same Name Override The Value Of The Previous One.
+        $merge_one = ["One" => "PHP", "Two" => "CSS", "Three" => "JavaScript"];
+        $merge_two = ["One" => "HTML", "Four" => "Python"];
+        print_r(array_merge($merge_one, $merge_two));  //Array ( [One] => HTML [Two] => CSS [Three] => JavaScript [Four] => Python )
+    Numeric Key Will Be Renumbered
+        $merge_three = [10 => "PHP", 20 => "CSS", 30 => "JavaScript"];
+        $merge_four = [40 => "Python", "10" => "Go"];
+        print_r(array_merge($merge_three, $merge_four)); //Array ( [0] => PHP [1] => CSS [2] => JavaScript [3] => Python [4] => Go ) 
+- array_replace(Array[Required], Replacement/s[Optional])=>Replaces Elements From Passed Arrays Into The First Array
+    Same Key => Value In Second Array Replace Same Key Regardless of data type=> Value In First Array
+    If Key In Second Array Not Found In Fisrt Array It Will Be Created
+        print_r(array_replace($merge_three, $merge_four)); //Array ( [10] => Go [20] => CSS [30] => JavaScript [40] => Python )
+
+
+- array_rand(Array[Required], Num[Optional])=> Pick [One , More=> Num parameter] Random Keys Out Of An Array
+      print_r(array_rand($nums, 3));
+- shuffle(Array[Required])=>Shuffle An Array It works on Array influence بتأثر ع الاراي ذات نفسها
+      shuffle($nums);  print_r($nums);
+
+
+- array_slice(Array[Required], Start[Required], Length[Optional], Preserve_Keys[Optional])=>Extract A Slice Of The Array
+    Length[Negative => Stop Slicing Until This Index منطقه محظورة, Not Set => All Elements From Start Position]
+    Preserve Keys[False => (Default) Reset Keys, True => Preserve Keys important for numeric Keys] If Array Have String Keys, It Will Always Preserve The Keys
+- array_splice(Array[Required], Start[Required], Length[Optional], Array[Optional])
+    Remove A Portion Of The Array And Replace It With Something Else, it works on Array influence
+
+- sort(Array[Required])=>Sort An Indexed Array In Ascending Order
+- rsort(Array[Required])=>Sort An Indexing Array In Descending Order
+- asort(Array[Required])=>Sort An Associative Array In Ascending Order According To The Value
+- arsort(Array[Required])=>Sort An Associative Array In Descending Order According To The Value
+- ksort(Array[Required])=>Sort An Associative Array In Ascending Order According To The Key
+- krsort(Array[Required])=>Sort An Associative Array In Descending Order According To The Key
+- natsort(Array[Required])=>Sorts An Array By Using A "Natural Order" Algorithm
+
+- array_product(Array[Required])=> Calculate The Product Of Values In An Array => Return Int || Float
+- array_sum(Array[Required])=>Calculate The Sum Of Values In An Array    
+*/
 
 $fruits = ['apple', 'banana', 'orange'];
-// Get array length
-echo count($fruits);
+
+
 // Search array
 echo in_array('banana', $fruits);
 
@@ -18,18 +93,22 @@ array_shift($fruits); // Removes first
 // Remove specific element with no shifting to indexes 
 unset($fruits[2]);          // 0 1 3 4
 
-// Split into chunks of 2  => htt2sm l aktr mn array kol array feha 2 elements
-$chunkedArray = array_chunk($fruits, 2);
-var_dump($chunkedArray[0]);
+/*array_chunk(Array[Required], Length[Required], Preserve_Key[Optional])
+    Split An Array Into Chunks [Return A Multidimensional Indexed Array]
+    Preserve_Key=> [False => Default Reindex The Keys, True Preserve The Keys if this associative array-> key=>value ]
+*/
+$countries = ["EG" => "Egypt", "KSA" => "Saudi Arabia", "Sy" => "Syria", "USA" => "United States"];
+$chunkedArray = array_chunk($countries, 2,true);
+echo '<pre>';
+print_r(array_chunk($countries, 2, True));
+echo '</pre>';
 
 // Concatenate arrays
 $arr1 = [1, 2, 3];
 $arr2 = [4, 5, 6];
-//$arr3==$arr4
-$arr3 = array_merge($arr1, $arr2);
-$arr4 = [...$arr1, ...$arr2]; // Use Spread =>allow iterable to Expand in place
+$arr3= [...$arr1, ...$arr2]; // Use Spread =>allow iterable to Expand in place
 
-// Combine arrays (Keys & values)
+// Combine arrays (Keys & values) they must have the same number of elements
 $a = ['green', 'red', 'yellow'];
 $b = ['avocado', 'apple', 'banana'];
 $c = array_combine($a, $b);
@@ -42,17 +121,25 @@ var_dump($flipped);
 
 // Create array of numbers with range()
 $numbers = range(1, 20);
-// Map through array and create a new one
-$newNumbers=array_map(function($number){
-    return 'Number '.$number.'<br>';
-},$numbers);
-// Filter array
-$lessThan10 = array_filter($numbers, fn($number) => $number < 10);
 
-// Array Reduce
-// "accumlator" holds the return value of the previous iterations
+// Map through array and create a new one
+//- array_map(Callback Function[Required], Array[Required], Other Arrays[Optional])
+$first_names = ["Osama", "Ahmed", "Sayed", "Mahmoud", "Sameh", "Gamal"];
+$last_names = ["Ameer", "Samy", "Shady", "Amr", "Mohamed", "Ibrahim"];
+print_r(array_map(fn($f, $l) => "Hello Mr $f $l", $first_names, $last_names));
+
+//array_filter(Array[Required], Callback Function[Required], Flag[Optional])
+//Flag[ARRAY_FILTER_USE_KEY,ARRAY_FILTER_USE_BOTH,0 Default => Send Value As Arguments]
+$nums = [1 => 3,6 => 1,3 => 2,4 => 8,5 => 4];
+function check_nums($key, $value) {
+    return $key> 4 || $value > 4;
+}
+print_r(array_filter($nums, "check_nums", ARRAY_FILTER_USE_BOTH));
+
+//array_reduce(Array[Required], Callback Function[Required], Initial_Value[Optional])=> Reduce The Array To A Single Value
+// "accumlator" holds the return value of the previous iterations || Initial value
 $sum = array_reduce($numbers, fn($accumlator, $current) => $accumlator + $current);
-var_dump($sum);
+echo $sum;
 
 
 $string2 = '<h1>Hello World</h1>';
--- 8_StringFunctions.php
+++ 8_StringFunctions.php
@@ -1,15 +1,92 @@
 <?php
 /* ---------- String Functions -------- */
-$string = 'Hello World';
+  /*
+    - lcfirst(String[Required]) =>  Make first letter of string is lowercase
+    - ucfirst(String[Required]) =>  Make first letter of string is uppercase
+    - str_repeat(String[Required], Count[Required])
+    - implode(Separator[Optional], Array[Required]) => join() Is Alias, 
+        merge whole Array to be one string with specific Seperator to seperate between elements
+    
+    - explode(Separator[Required], String[Required], Limit[Optional]) =>explode is reverse of implode, transfer string into Array but seperator here required.
+       limit is the length of array if [0,1]-> return one element
+       elseif more than one [3]->take what can it carry in [0,1] and in [2] take the rest of string
+       elseif negative[-2]->after explode function work then remove last two elements
+    
+    - str_shuffle(String[Required]) => random order of chars in string
+    
+    - trim(String[Required], CharsList[Optional]) =>Remove spaces from first and last parties 
+        echo trim("#Allam###", "##");=>some langauges will search for two hash one after the other and output #Allam#, but in PHP will remove all hashes output Allam 
+        echo trim("#@@Elzero@@@###", "@");=> noting will done because trim interact with only parties
+    
+    - chunk_split(String[Required], Length[Optional], End[Optional]) => chunk string to more than part
+        every part length is Length[Second parameter] default is 76
+        put in the end of part [Third parameter]      default is space
+    - str_split(String[Required], Length[Optional]) => split the string every Length of chars in element in array 
+    
+    - strip_tags(String[Required], Allowed[Optional]) =>prevent show of tags if i want to show some of tags put them in second parameter
+        echo strip_tags("<h3>Hello <b>Elzero</b></h3>", "<h3>,<b>");
+    - nl2br(String[Required], XHTML[Optional])=>[newline to br] replace \n by newline,XHTML for self closing tag if it true (default) [<br>Allam</br>]
+        echo nl2br("Elzero\nWeb\nSchool", false);  -> no self closing tag [Elzero<br>......]
+    
+    - strpos(String[Required], Value[Required], Start Position[Optional]) Case-Sensitive  ->first value
+    - strrpos(String[Required], Value[Required], Start Position[Optional]) Case-Sensitive ->last value
+    - stripos(String[Required], Value[Required], Start Position[Optional]) Case-Insensitive
+    - strripos(String[Required], Value[Required], Start Position[Optional]) Case-Insensitive
+        if no value the four functions will return false; space is included.
+        negative maybe to start position->start from end and reach to position and search for value from start to end
+   
+    - substr_count(String[Required], Value[Required], Start Position[Optional], Length[Optional])
+        return count of value is repeated in range[Start,length] start and length is indexes 
+        var_dump(substr_count("abcdabcda", "abcda")); // 1 because of Overlap String  
+    
+    - parse_str(String[Required], Array[Required])=>in the form query user enter input in fields
+        parse_str("name=Osama&#038;email=o@nn.sa&#038;os=mac", $query);
+        echo $query["name"] . $query["email"] . $query["os"];
+    - quotemeta(String[Required]) =>if user input tags maybe hack database this function put before special chars Escape char \
+        echo quotemeta("Hello [] () * +"); //Hello \[\] \(\) \* \+
+    - str_pad(String[Required], Length[Required], String[Optional], Pad Flag[Optional])
+       if i need to make serial meaning all serials is the same length
+       Flags[STR_PAD_BOTH=>(fifty left and the rest is right),STR_PAD_LEFT, STR_PAD_RIGHT]=>position of adding String to reach to the length of string
+       echo str_pad("500432", 8, 0, STR_PAD_LEFT);
+    
+    -strtr(String[Required], From[Required], To[Required]) || strtr(String[Required], Array[Required])=>String Translate from '--' to '--'
+      if i have one translate use first syntax
+        echo strtr("E@zero Web Schoo@", "@", "l");
+      but if i have more than translate use second syntax -> associative array key is from and value is to
+        $translation = ["@" => "l", "#" => "o"];
+        echo strtr("E@zer# Web Sch##@", $translation);
+    - str_replace(Find[Required], Replace With[Required], Data[Required], Replace Count[Optional])  Case-Sensitive for Find
+        find and replace maybe string OR Array each one and his opposition, if replace array less than find array this replace element will be empty
+        Replace count -> how much replace is occur
+         echo str_replace(["#", "@"], ["l", "o"], "E#zer@ Web Sch@@#", $r);
+    - str_ireplace(Find[Required], Replace With[Required], Data[Required], Replace Count[Optional]) Case-InSensitive for Find
 
+    - wordwrap(String[Required], Width[Optional = 75], Break_Char[Optional = "\n"], Cut_Long[Optional = False])
+         wraps a string into new lines when it reaches a specific length, you can to break with other char instead of newline
+         if we have long word and we need to wrap this word to newline make the last parameter True
+            echo wordwrap($str, 8, "<br>", True);
+    
+    - ord(String[Required])=>return ASCII CODE of char
+    - chr(Int[Required])=>return Char of ASCII CODE
+    - similar_text(String_One[Required], String_Two[Required], Percent[Optional]) 
+        Returns The Number Of Matching Characters
+        if i want to know the percentage of similar of two strings via the last parameter
+          echo similar_text("Elzero", "Elzeroo", $perc);
+    - strstr(String[Required], Search[Required], Before_Search[Optional] = False)=> Alias [strchr] Case-Sensitive
+    - stristr(String[Required], Search[Required], Before_Search[Optional] = False) Case-Insensitive
+        search for value of search in String and output from value to the end of String
+        if i want the whole previous of value, meaning from start of String to the value of search make the last parameter is TRUE
+    
+    
+    - number_format(Number[Required], Decimals[Optional], Decimal_String[Optional], Separator[Optional])
+        echo 10_000_000;=>Syntactic sugar   10000000 
+        echo number_format(10000000.156023, 3, "@", "A");   100A000A00@156
+
+    */
+  
+$string = 'Hello World';
 // Get the length of a string
 echo strlen($string);
-
-// Find the position of the first occurrence of a substring in a string
-echo strpos($string, 'o');
-
-// Find the position of the last occurrence of a substring in a string
-echo strrpos($string, 'o');
 
 // Reverse a string
 echo strrev($string);
@@ -21,7 +98,8 @@ echo strtolower($string);
 echo strtoupper($string);
 
 // Uppercase the first character of each word
-echo ucwords($string);
+//ucwords(String[Required], Delimiters[Optional]) => Delimiter here to split the string instead of space
+echo ucwords("elzero web|school", "|") . "<br>";  //Elzero web|School   char of w is no change because elzero web they consider one word
 
 // String replace
 echo str_replace('World', 'Everyone', $string);
